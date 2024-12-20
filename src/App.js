9759c9d51f2d7f8e27e687502cb6cc19afb74596
chakalisangeetha <chakalisangeetha2@gmail.com>
2024-12-20 09:18:59 +0530

    changing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("app" )
+  console.log("app changing")
   return (
     <div className="App">
       <header className="App-header">
