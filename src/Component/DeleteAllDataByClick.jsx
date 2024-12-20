bcfa4f1e854b05174c089b4a58c06b25314aa0f9
chakalisangeetha <chakalisangeetha2@gmail.com>
2024-12-20 11:41:31 +0530

    some changes

--- src/Component/DeleteAllDataByClick.jsx
+++ src/Component/DeleteAllDataByClick.jsx
@@ -1,7 +1,49 @@
-import react from 'react';
-const DeleteAllDataByClick=()=>{
-    return(
-        <>deleting the data</>
-    )
+import React from 'react';
+
+
+const DeleteAllDataByCilck = () => {
+    const DataList = [
+        { Name: 'rama', age: 23 },
+        { Name: 'gopal', age: 24 },
+        { Name: 'krishna', age: 27 },
+        { Name: 'goa', age: 25 },
+    ];
+
+    const [data, setData] = React.useState(DataList);
+
+    // Function to toggle between showing and hiding data
+    const toggleData = () => {
+        if (data.length === 0) {
+            setData(DataList); // If data is empty, restore it
+        } else {
+            setData([]); // If data is present, clear it
+        }
+    };
+
+    return (
+        <div>
+            <h1>Hello StackBlitz!</h1>
+            <p>Start editing to see some magic happen :)</p>
+
+            {/* Button to toggle data visibility */}
+            <button onClick={toggleData}>
+                {data.length === 0 ? 'Show data' : 'Delete data'}
+            </button>
+
+            <div>
+                {/* Display data only if data exists */}
+                {data.length > 0 ? (
+                    data.map((item, index) => (
+                        <div key={index}>
+                            <li>{item.Name}</li>
+                            <li>{item.age}</li>
+                        </div>
+                    ))
+                ) : (
+                    <p>No data available</p>
+                )}
+            </div>
+        </div>
+    );
 }
-export default DeleteAllDataByClick;
+export default DeleteAllDataByCilck;
