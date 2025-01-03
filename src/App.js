ce5e0715c397502665efbb4939b0f972be6afff9
chakalisangeetha <chakalisangeetha2@gmail.com>
2025-01-03 11:06:06 +0530

    pagenation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import DeleteAllDataByClick from './Component/DeleteAllDataByClick'
 import Disable from './Component/Disable'
 import Form from './Todo1/Form';
 import LocalStorage from './LocalStorages/Store';
+import Page from './Pagenation/Page'
 import './App.css';
-
 function App() {
   return (
     <div className="App">
+      <Page/>
       <LocalStorage/>
     <DeleteAllDataByClick/>
     <Disable/>
