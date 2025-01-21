8273836789a1d682ce7b9a6dbdbb4b45d7b02de8
chakalisangeetha <chakalisangeetha2@gmail.com>
2025-01-21 13:19:31 +0530

    readmoreandless

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
-import DeleteAllDataByClick from './DltAndDisable/DeleteAllDataByClick'
-import Disable from './DltAndDisable/Disable'
+import DeleteAllDataByClick from './DltalldatabyclickAndDisable/DeleteAllDataByClick'
+import Disable from './DltalldatabyclickAndDisable/Disable'
 import Page from './Pagenation/Page';
 import Store from './LocalStorages/Store';
 //  import Axios from './AxiosPostAndGet/Axios'
@@ -9,10 +9,12 @@ import Data from './Formik/Data';
   import Valid from './Validations/Valid'
 import Password from './PasswordGenerator/Password';
 import Async from './AsyncAndAwait/Async';
+import Read from './ReadMoreAndLess/Read'
 import './App.css';
 function App() {
   return (
     <div className="App">
+      <Read />
       <Async/>
       <Password/>
       {/* <Valid /> */}
