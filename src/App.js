56b5cfb476ee68ba43694bc54b149f3942d06c7f
chakalisangeetha <chakalisangeetha2@gmail.com>
2025-01-22 09:58:35 +0530

    ActiveOffers

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Read from './ReadMoreAndLess/Read';
 import Name from './UpdateSelInput/Name';
 import EditDlt from './OnlyEditAndDlt/EditDlt';
 import Ascending from './AscendingDecending/Ascending';
+import Active from './ActiveInActive/Active';
 function App() {
   return (
     <div className="App">
@@ -30,6 +31,7 @@ function App() {
       <Store />
       <DeleteAllDataByClick />
       <Disable />
+     < Active />
     </div>
   );
 }
