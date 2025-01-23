c650f10ced912ec4b223cd731ed61cacf3b3aa9a
chakalisangeetha <chakalisangeetha2@gmail.com>
2025-01-23 11:50:09 +0530

    IfCondition

---
+++ src/IfCondition/IfCndtn.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+const ifCndtn=()=>{
+const[status,setStatus]=useState(false)
+let text= "";
+if(status){
+text= "ON";
+}else{
+text="OFF";     
+}
+      return(
+            <>
+            <h1>IfConditions:</h1>
+                  <button onClick={(e)=>setStatus(!status)}>Click Me</button>
+                  status:{text}
+                 
+            </>
+      )
+}
+export default ifCndtn;
+
+
+
+
+
+
+
+
+
+
+
